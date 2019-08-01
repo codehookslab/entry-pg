7d897d67aba74c22cdbe6f69258d676dc4938b1c
codehooks <codehookshelp@gmail.com>
2019-08-01 13:10:52 +0530

    Updated console output in main method

--- EntryProject/src/com/codehooks/Test.java
+++ EntryProject/src/com/codehooks/Test.java
@@ -3,6 +3,6 @@ package com.codehooks;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Learning Git");
+		System.out.println("Learning Git is cool and fun");
 	}
 }
